c6de61109d820592a3d58d399a0c255f06ece733
spoconykocur420 <bbartekak777@gmail.com>
2023-11-23 13:31:07 +0100

    hdsfkjsafg

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello world!");
+        System.out.println("podaj ciąg:");
+
+        String ciag = scanner.nextLine();
+        String ciag1 ="";
+        String ciag2 ="";
+        if(ciag.length() >=2) {
+            ciag1 = ciag.substring((ciag.length() / 2) - 1,(ciag.length() / 2) +1);
+            ciag2 = ciag1.concat(ciag.substring(0,(ciag.length() / 2) -1) +ciag.substring(ciag.length() / 2) +1);
+
+            System.out.println(ciag1);
+        }else{
+            System.out.println("za krótki ciąg");
+        }
+
+
 
 
 
